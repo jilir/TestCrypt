07b12d0c7257d0f0337e5ccfd7fc46ba6b95092d
simpson474 <simpson474@9992ea2e-d1a9-4bfe-b82f-1a4385bed05d>
2011-01-16 14:25:02 +0000

    Added unhandled exception handler to avoid that the program exits quietly.

--- TestCrypt/FormMain.Designer.cs
+++ TestCrypt/FormMain.Designer.cs
@@ -167,7 +167,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FormMain";
-            this.Text = "TestCrypt 0.2.0";
+            this.Text = "TestCrypt 0.2.1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
--- TestCrypt/Program.cs
+++ TestCrypt/Program.cs
@@ -12,9 +12,31 @@ namespace TestCrypt
         [STAThread]
         static void Main()
         {
+            // register an exception handler which will handle all exceptions not directly handled within the program:
+            // without the exception handler the program will terminate silently and no one knows why
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
+        }
+
+        /// <summary>
+        /// The exception handler which will handle all exceptions not directly handled within the program.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = (Exception)e.ExceptionObject;
+                MessageBox.Show(string.Format("{1}{0}Stack Trace:{0}{2}", Environment.NewLine, ex.Message, ex.StackTrace), "Unhandled Exception", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            finally
+            {
+                Application.Exit();
+            }
         }
     }
 }
